98bb9f80345d99321ba05fc7c763b0905ffe0b94
oakwoodgates <wpguru4u@gmail.com>
2023-02-23 00:07:18 -0500

    setup chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from 'next/app'
+// setup chakra-ui
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  background: '#1F1F1F',
+  accent: '#833BBE',
+  bodyText: 'rgba(255, 255, 255, 0.75)',
 }
+
+const theme = extendTheme({ colors })
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
